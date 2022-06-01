22245f8ba66568590649b934776aae428dbc305f
Hendrik Nara <hendrik@Hendriks-MBP.attlocal.net>
2022-05-31 18:30:03 -0700

    lab thread

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,8 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t locks[NBUCKET];
+
 double
 now()
 {
@@ -51,7 +53,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(locks + i);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(locks + i);
   }
 }
 
@@ -102,6 +106,9 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
+  for (int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(locks + i, NULL);
+  }
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
